2d3417fb2b653e6bba6cae8467236ea1aede2fa7
Guo Xiang Tan <tgx_world@hotmail.com>
2014-09-26 09:34:21 +0800

    Cache data of each date with Redis.

--- app/controllers/dengue_data_controller.rb
+++ app/controllers/dengue_data_controller.rb
@@ -3,8 +3,14 @@ class DengueDataController < ApplicationController
 
   def coordinates
     begin
-      contents, metadata = dropbox_client.get_file_and_metadata("/#{params[:filename]}.csv")
-      coordinates = DengueDataParser.parse(contents)
+      if coordinates = $redis.get(params[:filename])
+        JSON.parse(coordinates)
+      else
+        contents, metadata = dropbox_client.get_file_and_metadata("/#{params[:filename]}.csv")
+        coordinates = DengueDataParser.parse(contents)
+        $redis.set(params[:filename], coordinates.to_json)
+        coordinates
+      end
     rescue => e
       e.message
     end
