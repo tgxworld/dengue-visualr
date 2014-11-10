c848366841dba2e25d44fe53d2d17cbc3c6ea8fa
Guo Xiang Tan <tgx_world@hotmail.com>
2014-11-10 21:30:04 +0800

    Fix maps not using the latest data.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
     dropbox_client = DropboxClient.new(Rails.application.secrets[:dropbox_access_token])
     metadata = dropbox_client.metadata('/')
 
-    @last_data = metadata['contents'].first['path']
+    @last_data = metadata['contents'].last['path']
       .scan(/(\d{2})_(\d{2})_(\d{4})/).first
 
     @last_data_date = @last_data.join('_')
